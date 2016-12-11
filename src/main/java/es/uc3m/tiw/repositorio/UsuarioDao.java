1a67f36c17e59578a07b6402e44349f9238f6a95
MarcosMesonero <100292865@alumnos.uc3m.es>
2016-12-11 18:39:07 +0000

    paquetes 2

--- src/main/java/es/uc3m/tiw/repositorio/UsuarioDao.java
+++ src/main/java/es/uc3m/tiw/repositorio/UsuarioDao.java
@@ -1,7 +1,7 @@
 package es.uc3m.tiw.repositorio;
 
 import java.util.List;
-import org.springframework.*;
+import org.springframework.data.repository.CrudRepository;
 import es.uc3m.tiw.dominios.Usuario;
 
 public interface UsuarioDao  extends CrudRepository<Usuario, Long> {
