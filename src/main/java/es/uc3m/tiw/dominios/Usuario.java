62f5ed42e35e8b059a07297b94f042b83712c900
MarcosMesonero <100292865@alumnos.uc3m.es>
2016-12-12 14:12:51 +0100

    login, delete, update y registro usuario

--- src/main/java/es/uc3m/tiw/dominios/Usuario.java
+++ src/main/java/es/uc3m/tiw/dominios/Usuario.java
@@ -1,5 +1,12 @@
 package es.uc3m.tiw.dominios;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
 
 @Entity
 @Table(name="USUARIOS")
