7b4792e989e22ed9368320114c8e45275ab91092
MarcosMesonero <100292865@alumnos.uc3m.es>
2016-12-16 06:54:25 +0100

    cambios menores

--- src/main/java/es/uc3m/tiw/ClienteSpringApplication.java
+++ src/main/java/es/uc3m/tiw/ClienteSpringApplication.java
@@ -1,29 +1,13 @@
 package es.uc3m.tiw;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import javax.annotation.PostConstruct;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import es.uc3m.tiw.Daos.AdministradorRepository;
-import es.uc3m.tiw.Daos.UsuarioRepository;
-import es.uc3m.tiw.dominios.Usuario;
-import es.uc3m.tiw.dominios.Administrador;
-
-
 @SpringBootApplication
 public class ClienteSpringApplication {
-
-
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ClienteSpringApplication.class, args);
 	}
-	
-
 
 }
